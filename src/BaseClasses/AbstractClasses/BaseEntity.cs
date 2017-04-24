dfe3f985d6c7d61de0f5dece634f4e80a9804cce
Joe Tjaarda <jtjaard@gmail.com>
2017-04-23 20:58:09 -0700

    Initial commit

---
+++ src/BaseClasses/AbstractClasses/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BaseClasses.AbstractClasses
+{
+    public abstract class BaseEntity
+    {        
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }        
+    }
+}
---
+++ src/DataAccessRepository/Contexts/DataAccessContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccessRepository
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<BookDataModel> Books { get; set; }
+
+        Task<int> SaveChanges();
+    }
+
+    public class DataAccessContext : DbContext, IDbContext
+    {
+        public DataAccessContext()
+        {            
+        }
+        public DataAccessContext(DbContextOptions<DataAccessContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TestDatabase;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookDataModel>().ToTable("Book");
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await SaveChangesAsync();
+        }
+
+        public DbSet<BookDataModel> Books { get; set; }
+    }
+}
